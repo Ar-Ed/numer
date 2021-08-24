5c023516abb5310aee7b207aa50c92707e6de119
Ar-Ed <rased27273333@gmail.com>
2021-08-24 15:35:15 +0300

    factorial impl

--- Cfiles/gmpp.c
+++ Cfiles/gmpp.c
@@ -1,21 +1,22 @@
 #include <gmp.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-void pfib(unsigned long long term_count){
+void pfib(unsigned long long term_count)
+{
 
     mpz_t a, b, temp;
     mpz_init(a);
     mpz_init(temp);
     mpz_init_set_si(b, 1);
 
-    for(unsigned long long i = 1; i < term_count; i++){
-
+    for (unsigned long long i = 1; i < term_count; i++)
+    {
         gmp_printf("\n%llu'th term is : %Zd", i, a);
 
         mpz_add(temp, a, b);
         mpz_set(a, b);
         mpz_set(b, temp);
-
     }
 
     gmp_printf("\n%llu'th term is : %Zd\n", term_count, a);
@@ -23,5 +24,22 @@ void pfib(unsigned long long term_count){
     mpz_clear(a);
     mpz_clear(b);
     mpz_clear(temp);
+}
 
+char *factorial(unsigned long term)
+{
+    mpz_t result;
+    mpz_init_set_si(result, 1);
+
+    for (unsigned long i = 1; i < term; i++)
+    {
+        mpz_mul_ui(result, result, i);
+    }
+
+    char *str = malloc(sizeof(char) * mpz_sizeinbase(result, 10));
+    mpz_get_str(str, 10, result);
+
+    mpz_clear(result);
+
+    return str;
 }
--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 import sys
-
+from time import time
 from numer.cmath import log10, cos, exp
-from numer.mpz import print_fib
+from numer.mpz import print_fib, factorial
 
 #print(cos(30.3321423, radian=False))
 #print()
-print_fib(int(sys.argv[1]))
+#print_fib(int(sys.argv[1]))
+
+
+def func():
+    factorial(int(sys.argv[1]))
+    print('finished')
+
+a = time()    
+func()
+b = time()
+print(b-a)
